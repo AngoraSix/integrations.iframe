be73b990d393a68de2b1f99d7251fc2ca706df63
Ger Roza <rozagerardo@gmail.com>
2024-04-15 23:58:28 -0300

    Fixed error in js file

--- static/js/trello/client.js
+++ static/js/trello/client.js
@@ -1,5 +1,7 @@
-const BLACK_ICON = 'https://iframe.integrations.angorasix.com/static/images/a6-250-black.png';
-const WHITE_ICON = 'https://iframe.integrations.angorasix.com/static/images/a6-250-white.png';
+const BLACK_ICON =
+  'https://iframe.integrations.angorasix.com/static/images/a6-250-black.png';
+const WHITE_ICON =
+  'https://iframe.integrations.angorasix.com/static/images/a6-250-white.png';
 
 TrelloPowerUp.initialize({
   'card-buttons': function (t, options) {
@@ -16,14 +18,16 @@ TrelloPowerUp.initialize({
       },
     ];
   },
-  "card-badges": function (t, options) {
-    return t.get('card', 'shared', 'capsParams')
-    .then(function(capsParams) {
-      const capsValue = capsParams.caps
-      return [{
-        icon: capsValue ? BLACK_ICON : WHITE_ICON,
-        text: capsParams.caps || 'No Caps',
-        color: capsValue ? null : 'red'
-      }];
+  'card-badges': function (t, options) {
+    return t.get('card', 'shared', 'capsParams').then(function (capsParams) {
+      const capsValue = capsParams.caps;
+      return [
+        {
+          icon: capsValue ? BLACK_ICON : WHITE_ICON,
+          text: capsParams.caps || 'No Caps',
+          color: capsValue ? null : 'red',
+        },
+      ];
     });
+  },
 });
